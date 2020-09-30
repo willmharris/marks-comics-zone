eca35ba28f563b76b73d8989fcbc3170b963b272
willmharris <willmharris0@gmail.com>
2020-09-30 17:29:46 -0400

    Updates

---
+++ precache-manifest.5767ad5ae8bffd7863e3df42d44665b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e081d54f9360a7c234249375e79e52",
+    "url": "/marks-comic-zone/index.html"
+  },
+  {
+    "revision": "0a7ffcfac3b6b2da263b",
+    "url": "/marks-comic-zone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "749befca6741fd8d08c5",
+    "url": "/marks-comic-zone/static/js/2.1da1c8c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marks-comic-zone/static/js/2.1da1c8c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a7ffcfac3b6b2da263b",
+    "url": "/marks-comic-zone/static/js/main.237f1e33.chunk.js"
+  },
+  {
+    "revision": "501ea32b2a0f8bb9b872",
+    "url": "/marks-comic-zone/static/js/runtime-main.70bcda13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marks-comic-zone/static/media/logo.5d5d9eef.svg"
+  }
+]);
